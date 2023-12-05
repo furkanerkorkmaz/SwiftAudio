a6f5a2f80d8ea13ba2e57fb4b3b91628555b7597
Furkan Erkorkmaz <erkorkmazfurkan@gmail.com>
2023-12-05 10:48:12 +0300

    SwiftAudio

---
+++ Sources/SwiftAudio/APError.swift
@@ -0,0 +1,20 @@
+//
+//  APError.swift
+//  SwiftAudio
+//
+//  Created by Jørgen Henrichsen on 25/03/2018.
+//
+
+import Foundation
+
+public struct APError {
+    enum LoadError: Error {
+        case invalidSourceUrl(String)
+    }
+
+    enum QueueError: Error {
+        case noPreviousItem
+        case noNextItem
+        case invalidIndex(index: Int, message: String)
+    }
+}
