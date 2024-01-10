8dcb4496354c8f8c976be3089ee7329ab95bcd9f
Furkan <erkorkmazfurkan@gmail.com>
2024-01-10 22:12:59 +0300

    watchOS

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftAudio",
+     platforms: [
+        .iOS(.v14),
+        .watchOS(.v6), // WatchOS sürümünü buraya ekleyin
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
